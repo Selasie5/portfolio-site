20e5b0a974f3a9ae462327e25565474a6adb3a01
SepenuSelasie <developerseps5@gmail.com>
2024-02-05 12:25:00 +0000

    Setup development environmnent

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+
+
+const App = () => {
+  return (
+    <div className="hero flex flex-col justify-center items-center">
+      <h1 className="text-primary uppercase text-3xl font-bold">Selasie Sepenu</h1>
+    </div>
+  )
+}
+
+export default App
